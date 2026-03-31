e0b26dc8b3a0afb55365ef0c79c1ce0948c98e24
lovely1236 <mycodingy@gmail.com>
2026-03-31 10:58:46 +0530

    (Lovely Pande)Refactored. Connection with live server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://quantitymeasurementapp-r98c.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
